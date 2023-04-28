73e8a7f7e2324d38af5a479fa5d22dcfa088b20c
shi.fengyuan <shi.fengyuan@xitang-ai.com>
2023-04-28 15:00:20 +0800

    add leap.nvim

---
+++ configs/leapconfig.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', 's', function ()
+  local focusable_windows_on_tabpage = vim.tbl_filter(
+    function (win) return vim.api.nvim_win_get_config(win).focusable end,
+    vim.api.nvim_tabpage_list_wins(0)
+  )
+  require('leap').leap { target_windows = focusable_windows_on_tabpage }
+end)
--- plugins.lua
+++ plugins.lua
@@ -6,6 +6,23 @@ local plugins = {
           require "custom.configs.lspconfig"
        end,
     },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = 
+        {
+         "vim","lua","html","css","javascript","json","c","cpp","python",
+         "c_sharp","gitignore","java",
+        },
+      },
+    },
+    {
+      "ggandor/leap.nvim",
+      config = function ( )
+         require "costom.config.leapconfig"
+      end
+
+    },
 }
 
 return plugins;
